8a47dc053fdbc9021a05f4ae428e584b1508bbd5
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-07-01 23:25:51 +0530

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.py
@@ -0,0 +1,61 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        def recursion(target):
+            if target == 0:
+                return 0
+            
+            if target < 0:
+                return float('inf')
+            
+            mini = float('inf')
+            for coin in coins:
+                ans = recursion(target - coin)
+
+                if ans != float('inf'):
+                    mini = min(mini, 1 + ans)
+            
+            return mini
+        
+        
+        def mem(target):
+            if target == 0:
+                return 0
+            
+            if target < 0:
+                return float('inf')
+            
+            if dp[target] != -1:
+                return dp[target]
+            
+            mini = float('inf')
+
+            for coin in coins:
+                ans = mem(target - coin)
+
+                if ans != float('inf'):
+                    mini = min(mini, 1 + ans)
+                
+            dp[target] = mini
+            return dp[target]        
+
+        def tabulisation():
+            dp = [float('inf')] * (amount + 1)
+            dp[0] = 0
+
+            for i in range(1, amount + 1):
+                for coin in coins:
+                    if i - coin >= 0 and dp[i - coin] != float('inf'):
+                        dp[i] = min(dp[i], 1 + dp[i - coin])
+            
+            return dp[-1]
+
+        # res = recursion(amount)
+        # return res if res != float('inf') else -1
+
+        # dp = [-1] * (amount + 1)
+        # res = mem(amount)
+        # return res if res != float('inf') else -1
+
+        res =  tabulisation()
+        return res if res != float('inf') else -1
