e39a4d3ffc9e306668f211bcd79b79544e428bee
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-07-15 10:22:29 +0530

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # Calc last index of each char
+        last_index = {}
+
+        for idx, ch in enumerate(s):
+            last_index[ch] = idx
+        
+        end = 0
+        ans = []
+        curr_len = 0
+        # Check if you've reached the last idx of any char - if so, append to ans
+        for idx, ch in enumerate(s):
+            curr_len += 1
+            end = max(end, last_index[ch])
+
+            if idx == end:
+                ans.append(curr_len)
+                end = 0
+                curr_len = 0
+        
+        return ans 
