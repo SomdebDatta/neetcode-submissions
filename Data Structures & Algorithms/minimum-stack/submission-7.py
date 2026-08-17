4ac8161464e0938bcaee5409a9eb4aab3ad6de18
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-08-17 23:48:46 +0530

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        self.mini = float('inf')
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        self.mini = min(self.mini, val)
+        self.min_stack.append(self.mini)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+        self.mini = self.min_stack[-1] if self.min_stack else float('inf')
+        
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mini
