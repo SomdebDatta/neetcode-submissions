4372193ed66f8b3ecbd9286952472b2a49be6073
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-08-24 23:20:01 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        
+        res = ""
+        lenRes = float("infinity")
+
+        t_map = defaultdict(int)
+        s_map = defaultdict(int)
+
+        for letter in t:
+            t_map[letter] += 1
+        
+        have, need = 0, len(t_map)
+        # print(t_map)
+        l, r = 0, 0
+
+        while r < len(s):
+            s_map[s[r]] += 1
+            if s_map[s[r]] == t_map[s[r]]:
+                have += 1
+            while have == need:
+                if (r - l + 1) < lenRes:
+                    res = s[l: r + 1]
+                    lenRes = r - l + 1
+                s_map[s[l]] -= 1
+                if s_map[s[l]] < t_map[s[l]]:
+                    have -= 1
+                l += 1
+            
+            r += 1
+        
+        return res
