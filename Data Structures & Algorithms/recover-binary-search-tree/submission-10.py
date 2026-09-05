835e6ea93e59036189d57c24b165daec8bdb9dfd
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-09-05 10:17:05 +0530

    Add: recover-binary-search-tree - submission-10

---
+++ Data Structures & Algorithms/recover-binary-search-tree/submission-10.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        bst_sorted = []
+
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+            bst_sorted.append(node)
+            dfs(node.right)
+        
+        dfs(root)
+
+        first = second = None
+
+        for i in range(len(bst_sorted) - 1):
+            if bst_sorted[i].val > bst_sorted[i + 1].val:
+                second = i + 1
+                if first is None:
+                    first = i
+                else:
+                    break
+        
+        bst_sorted[first].val, bst_sorted[second].val = bst_sorted[second].val, bst_sorted[first].val
