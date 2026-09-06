e4a2287dcd06752373b6eb8e8ea7b1f6a822af3b
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-09-07 00:58:00 +0530

    Add: min-cost-climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        cache = [-1] * (len(cost) + 1)
+        
+        def recursion(curr):
+            if curr == 0 or curr == 1:
+                return cost[curr]
+            
+            if cache[curr] != -1:
+                return cache[curr]
+            
+
+            
+            cache[curr] = cost[curr] + min(recursion(curr - 1), recursion(curr - 2))
+            return cache[curr]
+        
+        return recursion(len(cost) - 1)
+        
