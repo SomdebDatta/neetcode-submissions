07fbb356e97307e0d726b7e3d16ea39e4f9cc400
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-09-07 00:22:53 +0530

    Add: climbing-stairs - submission-15

---
+++ Data Structures & Algorithms/climbing-stairs/submission-15.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = [-1] * (n + 1)
+
+        def recursion(curr):
+            if curr == n:
+                return 1
+            
+            if curr > n:
+                return 0
+            
+            if cache[curr] != -1:
+                return cache[curr]
+
+            
+            cache[curr] = recursion(curr + 1) + recursion(curr + 2)
+            return cache[curr]
+
+        return recursion(0)
+            
+
