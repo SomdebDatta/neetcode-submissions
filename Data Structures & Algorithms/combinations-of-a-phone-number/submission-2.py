aca577b2fcc24075c488ba55d12dfdfd58608351
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-09-06 18:49:54 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hashmap = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+
+        ans = []
+
+        for digit in digits:
+            if not ans:
+                ans.extend(list(hashmap[digit]))
+            else:
+                new_ans = []
+                for word in ans:
+                    for letter in hashmap[digit]:
+                        new_ans.append(word + letter)
+                ans = new_ans
+        
+        return ans
