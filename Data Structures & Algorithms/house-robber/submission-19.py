ef7b839fd479131884ddd38d3a6205956a1e78ce
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-09-07 06:56:09 +0530

    Add: house-robber - submission-19

---
+++ Data Structures & Algorithms/house-robber/submission-19.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = [-1] * len(nums)
+        
+        def recursion(curr):
+            if curr >= len(nums):
+                return 0
+            
+            if cache[curr] != -1:
+                return cache[curr]
+            
+            cache[curr] = max(nums[curr] + recursion(curr + 2), recursion(curr + 1))
+            return cache[curr]
+        
+        # return max(recursion(0), recursion(1))
+        return recursion(0)
