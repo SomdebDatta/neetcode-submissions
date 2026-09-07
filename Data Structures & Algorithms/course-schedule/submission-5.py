a649fb0c56e3376f8a1009a5f4fca9bdc104b141
Somdeb Datta <60882209+SomdebDatta@users.noreply.github.com>
2026-09-07 14:52:34 +0530

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = defaultdict(list)
+
+        for a, b in prerequisites:
+            adj_list[a].append(b)
+        
+        seen = set()
+        
+        def dfs(node):
+            if adj_list[node] == []:
+                return True
+
+            if node in seen:
+                return False
+            
+            seen.add(node)
+            
+            for pre in adj_list[node]:
+                if not dfs(pre):
+                    return False
+            adj_list[node] = []
+            # seen.remove(node)
+            return True
+        
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+        
+        return True
